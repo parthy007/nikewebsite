3eabac766104a1cefe528db9622068262a9d5af7
parth adhikari <parthadhikari1812@gmail.com>
2024-02-26 00:08:26 +0530

    added new props

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,14 +1,32 @@
 import React from "react";
 
-const Button = ({ label, iconURL }) => {
+const Button = ({
+  label,
+  iconURL,
+  backgroundColor,
+  borderColor,
+  textColor,
+  fullWidth,
+}) => {
   return (
-    <button className="flex gap-2 justify-center items-center bg-coral-red px-7 py-4 rounded-full text-white font-montserrat border border-coral-red leading-none">
+    <button
+      className={`flex gap-2 justify-center items-center  px-7 py-4 rounded-full  font-montserrat leading-none border ${
+        fullWidth && "w-full"
+      }
+      ${
+        backgroundColor
+          ? `${backgroundColor} ${textColor} ${borderColor}`
+          : "border-coral-red text-white bg-coral-red "
+      }}`}
+    >
       {label}
-      <img
-        src={iconURL}
-        alt="arrow right icon"
-        className="w-5 h-5 rounded-full ml-2"
-      />
+      {iconURL && (
+        <img
+          src={iconURL}
+          alt="arrow right icon"
+          className="w-5 h-5 rounded-full ml-2"
+        />
+      )}
     </button>
   );
 };
